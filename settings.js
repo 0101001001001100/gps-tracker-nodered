2b05170d3d88a8c4325892f4724f6d2c14ae7479
0101001001001100 <88755736+0101001001001100@users.noreply.github.com>
2025-08-03 17:14:34 +0200

    Mise à jour du mot de passe haché pour admin

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ module.exports = {
         type: "credentials",
         users: [{
             username: "admin",
-            password: "$2b$08$M3nt6sjEG5EkZlTCMXPZ5e.8xYHacItJzovG3KPXBv95xLtYu2cYO" // "superpass2025"
+            password: "$2b$08$zjHt1Y79X7CKmjvMyV3lzOZoxYa8qZuAglXVtHntmdBHaVzSG3K2e" // "superpass2025"
             permissions: "*"
         }]
     }
