91c2cf3eeb439b0aacbf0f07c2b383aaf426e073
0101001001001100 <88755736+0101001001001100@users.noreply.github.com>
2025-08-03 17:58:37 +0200

    Fix : correction finale du settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ module.exports = {
         type: "credentials",
         users: [{
             username: "admin",
-            password: "$2b$08$zjHt1Y79X7CKmjvMyV3lzOZoxYa8qZuAglXVtHntmdBHaVzSG3K2e" // "superpass2025"
+            password: "$2b$08$zjHt1Y79X7CKmjvMyV3lzOZoxYa8qZuAglXVtHntmdBHaVzSG3K2e", // "superpass2025"
             permissions: "*"
         }]
     }
