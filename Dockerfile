18780e8b641e64ea6ad03c1010d027f279d2ac25
0101001001001100 <88755736+0101001001001100@users.noreply.github.com>
2025-08-03 16:00:15 +0200

    Add files via upload

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+
+FROM nodered/node-red:4.0
+
+# Installer les modules nécessaires
+RUN npm install     node-red-dashboard     node-red-contrib-web-worldmap     jsonwebtoken
+
+# Copier les flows
+COPY flows.json /data/flows.json
+COPY settings.js /data/settings.js
---
+++ settings.js
@@ -0,0 +1,10 @@
+module.exports = {
+    adminAuth: {
+        type: "credentials",
+        users: [{
+            username: "admin",
+            password: "$2b$08$dFV7aY.9J0c9TW3TbHJX..QnMxolB0T8QJ4UpHzk93BgIYfS18Z8i", // superpass2025
+            permissions: "*"
+        }]
+    }
+};
